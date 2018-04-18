0189e4016b718772082ad3d81283695242b9c900
Gilles Chris <progchris@live.fr>
2018-04-19 01:41:23 +0200

    formulaire add todoList

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const btn = document.getElementsByTagName('span');
+const btnAdd = document.getElementsByClassName('add');
+console.log(btnAdd)
 
 for(var i=0; i<btn.length; i++) {
-  btn[i].addEventListener("click", function(){
+  btn[i].addEventListener("click",
+  function(){
     this.parentNode.remove().fadeOut();
   });
-}
+};
+
+btnAdd[0].addEventListener("click",
+  function(e){
+    e.preventDefault();
+    let pop = document.getElementById('popup');
+    pop.classList.remove('hide')
+    pop.classList.add('show')
+  }
+);

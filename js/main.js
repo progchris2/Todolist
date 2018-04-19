d67b43bd508bdb0aa92f0c2bb9ef49bb794113a1
Gilles Chris <progchris@live.fr>
2018-04-19 14:51:10 +0200

    add compteur

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 const btn = document.getElementsByTagName('span');
 const btnAdd = document.getElementsByClassName('add');
 const close = document.getElementsByClassName('close ');
-console.log(close);
+const msg = document.getElementById('count');
+const cpt = document.getElementById('cpteur')
+
+console.log(btnAdd);
 
 for(var i=0; i<btn.length; i++) {
   btn[i].addEventListener("click",
   function(){
     this.parentNode.remove();
+    compteur();
   });
 };
+/*
+const count = document.createElement('p');
+msg.appendChild(count).classList.add('todo-2');
+const newText = document.createTextNode(btn.length);
+count.appendChild(newText);
+*/
+function compteur() {
+  cpt.textContent = btn.length.toString();
+}
 
 btnAdd[0].addEventListener("click",
   function(e){

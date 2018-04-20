19ef97488a8164d0a5e6b12a0c339480b032a6c7
Gilles Chris <progchris@live.fr>
2018-04-20 21:09:00 +0200

    function add items

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 const btn = document.getElementsByTagName('span');
-const btnAdd = document.getElementsByClassName('add');
-const close = document.getElementsByClassName('close ');
 const msg = document.getElementById('count');
-const cpt = document.getElementById('cpteur')
+const cpt = document.getElementById('cpteur');
+const pop = document.getElementById('popup');
 
-console.log(btnAdd);
-
+cpt.innerHTML = btn.length.toString();
 for(var i=0; i<btn.length; i++) {
   btn[i].addEventListener("click",
   function(){
     this.parentNode.remove();
-    compteur();
+    cpt.innerHTML = btn.length.toString();
   });
 };
 /*
@@ -19,21 +17,89 @@ msg.appendChild(count).classList.add('todo-2');
 const newText = document.createTextNode(btn.length);
 count.appendChild(newText);
 */
-function compteur() {
-  cpt.textContent = btn.length.toString();
-}
 
-btnAdd[0].addEventListener("click",
+document.getElementsByClassName('add')[0].addEventListener("click",
   function(e){
     e.preventDefault();
-    let pop = document.getElementById('popup');
     pop.classList.remove('hide')
     pop.classList.add('show')
   }
 );
 
-close[0].addEventListener("click",
-  function(e) {
-    alert();
+// function pop up view
+document.getElementsByClassName('add')[0].addEventListener("click",
+  function(e){
+    e.preventDefault();
+    this.parentNode.classList.remove('hide');
+    pop.classList.remove('hide');
+    pop.classList.add('show');
   }
 );
+
+// function pop up close
+document.getElementById('close').addEventListener("click",
+  function(e) {
+    e.preventDefault();
+    pop.classList.remove('show');
+    pop.classList.add('hide');
+  }
+);
+
+//add datas
+document.getElementById('btn-add').addEventListener("click",
+  function(e) {
+    e.preventDefault();
+    let theme = document.getElementById('theme').value;
+    let image = document.getElementById('img').value;
+    let texte = document.getElementById('texte').value;
+
+    createElementDom(image, theme, texte);
+    cpt.innerHTML = btn.length.toString();
+
+    pop.classList.remove('show');
+    pop.classList.add('hide');
+  }
+);
+
+function createElementDom(el, elt, elmt) {
+  //create parent div
+  let div = document.createElement('div');
+  document.getElementById('section').appendChild(div).classList.add('block-list');
+
+
+  //create first child div: img
+  let img = document.createElement('img');
+  div.appendChild(img).classList.add('avatar');
+  img.src = document.createTextNode(el);
+
+  //create second child div: span
+  let divChild = document.createElement('div');
+  div.appendChild(divChild).classList.add('list');
+
+  let h2 = document.createElement('h2');
+  divChild.appendChild(h2);
+  h2.appendChild(document.createTextNode(elt));
+
+  let p = document.createElement('p');
+  divChild.appendChild(p);
+  p.appendChild(document.createTextNode(elmt));
+
+  let span = document.createElement('span');
+  div.appendChild(span);
+  span.addEventListener('click',
+    function() {
+      cpt.innerHTML = (btn.length - 1).toString();
+      this.parentNode.remove();
+    }
+  );
+
+  //create trash: i
+  let elementI = document.createElement('i')
+  span.appendChild(elementI);
+  elementI.className = "fas fa-trash elem";
+
+
+  document.getElementById('theme').value = '';
+  document.getElementById('img').value = '';
+  document.getElementById('texte').value = '';
+}
